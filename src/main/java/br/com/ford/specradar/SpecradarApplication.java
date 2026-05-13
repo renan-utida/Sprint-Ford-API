d068d04265b17b206be3c2e3e3eb030ce7303f77
Renan <renandiutida@gmail.com>
2026-05-12 21:34:55 -0300

    Init: Começando o nosso projeto, configurando as dependencias no pom.xml e organizando em pastas para a arquitetura em camadas

---
+++ src/main/java/br/com/ford/specradar/SpecradarApplication.java
@@ -0,0 +1,13 @@
+package br.com.ford.specradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ford/specradar/SpecradarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ford.specradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
